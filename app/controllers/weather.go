af3db0a4672daf1181fc1cd9d4802cea5fff2c14
JackSpirou <jack.spirou@me.com>
2015-05-17 00:22:16 -0500

    fixing some comments

--- app/controllers/weather.go
+++ app/controllers/weather.go
@@ -9,32 +9,35 @@ import (
 // WeatherController contains controller logic for the weather endpoint.
 type WeatherController struct{}
 
+// Create a weather controller.
 var Weather = &WeatherController{}
 
 // Show handles a "/weather/:city" GET request for the WeatherController.
 func (wc *WeatherController) Show(resp gf.Responder, req *gf.Request) {
 
+	// get the city as a parameter
 	city := req.Param("city")
 
+	// try some basic input checking
 	if len(city) < 3 {
 		resp.WriteErrs(gf.InvalidInput, "not a valid city name")
 		return
 	}
 
+	// use the weather service to get the weather
 	w, err := weather.Find(city, "us")
 
+	// check for errors
 	if err != nil {
 
-		// Add json body data.
+		// write a response
 		resp.WriteJSON(map[string]string{
 			"hello " + city: "Sorry, no weather report today. :( ",
 			"error":         err.Error(),
 		})
-
 		return
 	}
 
-	// Add json body data.
+	// write the weather data
 	resp.Write(req, w)
-
 }
