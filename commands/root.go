6615cc329f5c32c355d4425a10b13eeadcb29d3d
JackSpirou <jack.spirou@me.com>
2015-07-27 20:42:07 -0500

    thinking of giving cobra a go

---
+++ commands/root.go
@@ -0,0 +1,14 @@
+package commands
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "Gophersaurus App",
+	Short: "Gophersaurus is a RESTful API framework for Golang services.",
+	Long: `Gophersaurus is a RESTFul API framework
+          that provides a solid directory structure
+          to organize a MVC application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
