a0b4a0f455f6de7c3819931a0377d7e85058e64c
JackSpirou <jack.spirou@me.com>
2015-07-28 21:53:08 -0500

    updating default framework cli message

--- commands/root.go
+++ commands/root.go
@@ -3,9 +3,9 @@ package commands
 import "github.com/spf13/cobra"
 
 var RootCmd = &cobra.Command{
-	Use:   "gophersaurus App",
-	Short: "gophersaurus is a RESTful API framework for Golang services.",
+	Use:   "gophersaurus application",
+	Short: "gophersaurus is a RESTful API framework for building go services.",
 	Long: `gophersaurus is a RESTFul API framework
-          that provides a solid directory structure
-          to organize a MVC application.`,
+          that provides a solid structure for developing and organizing MVC
+					application services.`,
 }
