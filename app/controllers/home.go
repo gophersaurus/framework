6fd40640f47a78cd96725f8d09de023450b4f8ce
JackSpirou <jack.spirou@me.com>
2015-07-28 21:59:20 -0500

    explicit controller method calls with indentation

--- app/controllers/home.go
+++ app/controllers/home.go
@@ -5,27 +5,29 @@ import "github.com/gophersaurus/gf.v1/http"
 // Home is a HomeController.
 var Home = struct {
 	Index func(resp http.Responder, req *http.Request)
-}{func(resp http.Responder, req *http.Request) {
-
-// set the default HTTP scheme without SSL/TLS
-scheme := "http://"
-
-// check if we are serving SSL/TLS HTTP traffic
-if req.TLS != nil {
-	scheme = "https://"
-}
-
-// define an anonymous result struct
-result := struct {
-	Status     int    `json:"status" xml:"status"`
-	Message    string `json:"message" xml:"message"`
-	PublicPage string `json:"public_page" xml:"public_page"`
 }{
-	200,
-	"Welcome fellow gopher.",
-	scheme + req.Host + "/public",
-}
+	Index: func(resp http.Responder, req *http.Request) {
 
-// write the result
-resp.Write(req, result)
-}}
+	// set the default HTTP scheme without SSL/TLS
+	scheme := "http://"
+
+	// check if we are serving SSL/TLS HTTP traffic
+	if req.TLS != nil {
+		scheme = "https://"
+	}
+
+	// define an anonymous result struct
+	result := struct {
+		Status     int    `json:"status" xml:"status"`
+		Message    string `json:"message" xml:"message"`
+		PublicPage string `json:"public_page" xml:"public_page"`
+	}{
+		200,
+		"Welcome fellow gopher.",
+		scheme + req.Host + "/public",
+	}
+
+	// write the result
+	resp.Write(req, result)
+	},
+}
--- app/controllers/weather.go
+++ app/controllers/weather.go
@@ -8,28 +8,30 @@ import (
 
 var Weather = struct {
 	Show func(resp http.Responder, req *http.Request)
-}{func(resp http.Responder, req *http.Request) {
+}{
+	Show: func(resp http.Responder, req *http.Request) {
 
-	// get the city as a parameter
-	city := req.Param("city")
+		// get the city as a parameter
+		city := req.Param("city")
 
-	// try some basic input checking
-	if len(city) < 3 {
-		resp.WriteErrs(req, http.InvalidInput, "not a valid city name")
-		return
-	}
+		// try some basic input checking
+		if len(city) < 3 {
+			resp.WriteErrs(req, http.InvalidInput, "not a valid city name")
+			return
+		}
 
-	// use the weather service to get the weather
-	w, err := weather.Find(city, "us")
+		// use the weather service to get the weather
+		w, err := weather.Find(city, "us")
 
-	// check for errors
-	if err != nil {
+		// check for errors
+		if err != nil {
 
-		// write a response
-		resp.WriteErrs(req, "Sorry, no weather report today...", err.Error())
-		return
-	}
+			// write a response
+			resp.WriteErrs(req, "Sorry, no weather report today...", err.Error())
+			return
+		}
 
-	// write the weather data
-	resp.Write(req, w)
-}}
+		// write the weather data
+		resp.Write(req, w)
+	},
+}
